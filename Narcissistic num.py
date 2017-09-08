75ce122682c5fce4bf040e5d6ce5c585ec399051
Sally <songqi26@outlook.com>
2017-09-08 17:19:18 -0400

    find string

--- Narcissistic num.py
+++ Narcissistic num.py
@@ -6,5 +6,5 @@ def Narcissistic(): #153=1**3 + 5**3 +3**3
             sum = sum + (temp % 10) ** 3
             temp = temp // 10
         if sum == each:
-            print(each)
+            print(each, end='\t')
 print(Narcissistic())
