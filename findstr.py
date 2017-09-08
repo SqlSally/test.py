48897eb1ea381bb92e493f47d56d564d24f72463
Sally <songqi26@outlook.com>
2017-09-08 18:28:01 -0400

    find string

--- findstr.py
+++ findstr.py
@@ -6,6 +6,8 @@
 
 #a:  usestr
 #b:  detaistr
+
+'''
 def findstr(destr, substr):
     length = len(destr)
     count = 0
@@ -19,3 +21,24 @@ def findstr(destr, substr):
     else:
         print("no")
 print(findstr("imimim", "im"))
+'''
+
+
+def findstr(substr, destr):  # 思路是把 substr的每一个字母，对应 destr中的每一个字母，先实现第一种，在实现接下来那种
+    length = len(destr)
+    count = 0
+    if substr in destr:
+        for each in range(length-1):  # 关于length 需要减去1
+            if substr[0] == destr[each]:
+                if substr[1] == destr[ each + 1 ]:
+                    count += 1
+        return count
+    else:
+        print("no")
+
+print(findstr("lo", "lovelololololol"))
+print(len("lovelololololo"))
+for each in range(len("lovelololololol")-1):
+    print(each)
+
+
